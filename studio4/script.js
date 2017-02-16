ff68a1d6f23ef321c0b412aac4a2dc2794cd998f
charlesdmak <charlesmak100@gmail.com>
2017-02-16 01:20:51 -0800

    iseaproposal

--- studio4/script.js
+++ studio4/script.js
@@ -13,48 +13,107 @@ document.addEventListener("DOMContentLoaded", function() {
     var popout90 = document.getElementById("popout90");
     var decades00 = document.getElementById("decades00");
     var popout00 = document.getElementById("popout00");
+    var pic70 = document.getElementById("pic70");
+    var pic80 = document.getElementById("pic80");
+    var pic90 = document.getElementById("pic90");
+    var pic00 = document.getElementById("pic00");
 
     //first section - hover: change color, popout, timer for tooltip // click: play music
     //add eventListener for hover
     decades70.addEventListener("mouseover", function(){
-      decades70.style.backgroundColor="green";
+    //  decades70.style.backgroundColor="green";
+      pic70.style.display="block";
       popout70.style.display="block";
     });
 
     decades70.addEventListener("mouseout", function(){
-      decades70.style.backgroundColor="red";
+      decades70.style.backgroundColor="grey";
+      pic70.style.display="none";
       popout70.style.display="none";
     });
 
+    pic70.addEventListener("click", function(){
+    //  decades70.style.backgroundColor="green";
+      pic70.style.display="block";
+      popout70.style.display="block";
+    });
+
     decades80.addEventListener("mouseover", function(){
-      decades80.style.backgroundColor="green";
+    //  decades80.style.backgroundColor="green";
+      pic80.style.display="block";
       popout80.style.display="block";
     });
 
     decades80.addEventListener("mouseout",function(){
-      decades80.style.backgroundColor="red";
+      decades80.style.backgroundColor="grey";
+      pic80.style.display="none";
       popout80.style.display="none";
     });
 
     decades90.addEventListener("mouseover", function(){
-      decades90.style.backgroundColor="green";
+    //  decades90.style.backgroundColor="green";
+      pic90.style.display="block";
       popout90.style.display="block";
     });
 
     decades90.addEventListener("mouseout",function(){
-      decades90.style.backgroundColor="red";
+      decades90.style.backgroundColor="grey";
+      pic90.style.display="none";
       popout90.style.display="none";
     });
 
     decades00.addEventListener("mouseover", function(){
-      decades00.style.backgroundColor="green";
+    //  decades00.style.backgroundColor="green";
+      pic00.style.display="block";
       popout00.style.display="block";
     });
 
     decades00.addEventListener("mouseout",function(){
-      decades00.style.backgroundColor="red";
+      decades00.style.backgroundColor="grey";
+      pic00.style.display="none";
       popout00.style.display="none";
     });
+
+decades70.addEventListener ('mouseover', function(){
+  tipTimer = setTimeout(showTip, 1000);
+});
+
+decades70.addEventListener ('mouseout', function(){
+  clearTimeout(tipTimer);
+  tip70.style.opacity = 0;
+});
+
+decades80.addEventListener ('mouseover', function(){
+  tipTimer = setTimeout(showTip, 1000);
+});
+
+decades80.addEventListener ('mouseout', function(){
+  clearTimeout(tipTimer);
+  tip80.style.opacity = 0;
+});
+
+decades90.addEventListener ('mouseover', function(){
+  tipTimer = setTimeout(showTip, 1000);
+});
+
+decades90.addEventListener ('mouseout', function(){
+  clearTimeout(tipTimer);
+  tip90.style.opacity = 0;
+});
+
+decades00.addEventListener ('mouseover', function(){
+  tipTimer = setTimeout(showTip, 1000);
+});
+
+decades00.addEventListener ('mouseout', function(){
+  clearTimeout(tipTimer);
+  tip00.style.opacity = 0;
+});
+
+function showTip () {
+  tip70.style.opacity = .8;
+  tip70.style.transition = 'all .5s';
+};
 
 
 });
